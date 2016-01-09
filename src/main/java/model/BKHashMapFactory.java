e2b82f457156d8dfae165e7261b553974c2218ba
AnnaBrylyakova <anna.brylyakova@phystech.edu>
2016-01-09 22:30:10 +0300

    factory returns map interface instead of impl, need for furtherde-privating and overriding methods

--- src/main/java/model/BKHashMapFactory.java
+++ src/main/java/model/BKHashMapFactory.java
@@ -3,32 +3,30 @@ package model;
 import model.map.impls.FineGrainedBKHashMap;
 import model.map.impls.GlobalLockBKHashMap;
 import model.map.impls.SynchronizedBKHashMap;
+import model.map.naive.BKMap;
 
 public class BKHashMapFactory {
-    public static final int DEFAULT_SIZE = 16;
     public static final int DEFAULT_CAPACITY = 16;
 
-    private final int size;
     private final int capacity;
 
-    public BKHashMapFactory(int size, int capacity){
-        this.size = size;
+    public BKHashMapFactory(int capacity){
         this.capacity = capacity;
     }
 
     public static BKHashMapFactory init() {
-        return new BKHashMapFactory(DEFAULT_SIZE, DEFAULT_CAPACITY);
+        return new BKHashMapFactory(DEFAULT_CAPACITY);
     }
 
-    public SynchronizedBKHashMap synchronizeed() {
-        return new SynchronizedBKHashMap(size, capacity);
+    public BKMap synchronizeed() {
+        return new SynchronizedBKHashMap(capacity);
     }
 
-    public GlobalLockBKHashMap globalLock() {
-        return new GlobalLockBKHashMap(size, capacity);
+    public BKMap globalLock() {
+        return new GlobalLockBKHashMap(capacity);
     }
 
-    public FineGrainedBKHashMap fineGrained() {
-        return new FineGrainedBKHashMap(size, capacity);
+    public BKMap fineGrained() {
+        return new FineGrainedBKHashMap(capacity);
     }
 }
